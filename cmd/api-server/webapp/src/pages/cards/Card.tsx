bf20a1d22edf6794abeda316d8733a64c7a14eac
Czar Milan <info@ux-studio.com>
2024-06-15 15:17:00 +0200

    update

--- cmd/api-server/webapp/src/pages/cards/Card.tsx
+++ cmd/api-server/webapp/src/pages/cards/Card.tsx
@@ -1,16 +1,40 @@
 import { LockClosedIcon, LockOpenIcon } from "@heroicons/react/16/solid";
 import { useState } from "react";
+import { Button } from "../../ui/button";
 
 type Props = {
   card: Card;
+  callback: () => void;
 };
-export default function Card({ card }: Props) {
+
+export default function Card({ card, callback }: Props) {
   const [answerLocked, setAnswerLocked] = useState(true);
+
+  const sendResult = async (success: boolean) => {
+    const q = await fetch("/api/result", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        CardID: card.ID,
+        Success: success,
+      }),
+    });
+
+    if ((await q).ok) {
+      if (callback !== undefined) {
+        callback();
+      }
+      setAnswerLocked(true);
+    }
+  };
+
   return (
     <div className="border rounded-lg shadow p-4 mb-4">
       <div>{card.QuestionText}</div>
 
-      <div className="mt-2">
+      <div className="mt-4">
         <div
           onClick={() => setAnswerLocked((prev) => !prev)}
           className="flex items-center gap-1 select-none cursor-pointer hover:bg-gray-200 p-2 rounded-lg"
@@ -27,7 +51,20 @@ export default function Card({ card }: Props) {
         </div>
         {!answerLocked && <div>{card.AnswerText}</div>}
       </div>
-      {!answerLocked && <div className="mt-2">ok / nem ok</div>}
+      {!answerLocked && (
+        <div className="mt-4 flex gap-1">
+          <div>
+            <Button color={"red"} onClick={() => sendResult(false)} className="cursor-pointer">
+              Fail
+            </Button>
+          </div>
+          <div>
+            <Button color={"green"} onClick={() => sendResult(true)} className="cursor-pointer">
+              Success
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
