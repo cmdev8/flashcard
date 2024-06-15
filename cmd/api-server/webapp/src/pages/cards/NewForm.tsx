fe8f1f90c65fa1568f7972c55dc82ad6facd775a
Czar Milan <info@ux-studio.com>
2024-06-15 14:31:51 +0200

    update

--- cmd/api-server/webapp/src/pages/cards/NewForm.tsx
+++ cmd/api-server/webapp/src/pages/cards/NewForm.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Field, Label } from "../../ui/fieldset";
 import { Textarea } from "../../ui/textarea";
 import { Button } from "../../ui/button";
+import { PlusCircleIcon, XCircleIcon } from "@heroicons/react/16/solid";
 
 type Props = {
   category: string;
@@ -11,22 +12,22 @@ type Props = {
 export default function NewCardForm({ category, afterCreate }: Props) {
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
-
+  const [open, setOpen] = useState(false);
 
   const save = async () => {
-    const q = await fetch('/api/card', {
-      method: 'POST',
+    const q = await fetch("/api/card", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         Category: category,
         QuestionText: question,
-        AnswerText: answer
+        AnswerText: answer,
       }),
     });
 
-    if((await q).ok) {
+    if ((await q).ok) {
       setQuestion("");
       setAnswer("");
     }
@@ -35,38 +36,55 @@ export default function NewCardForm({ category, afterCreate }: Props) {
   };
 
   return (
-    <div className="border rounded-lg shadow p-4">
-      <div>
-        <span className="font-bold">{category}</span> category
-      </div>
-
-      <div className="mt-2">
-        <Field>
-          <Label>Question</Label>
-          <Textarea
-            value={question}
-            onChange={(e) => setQuestion(e.target.value)}
-          />
-        </Field>
-      </div>
-
-      <div className="mt-2">
-        <Field>
-          <Label>Answer</Label>
-          <Textarea
-            value={answer}
-            onChange={(e) => setAnswer(e.target.value)}
-          />
-        </Field>
-      </div>
-
-      <div className="mt-2 flex justify-end">
+    <div className="border rounded-lg shadow p-4 select-none">
+      <div
+        onClick={() => setOpen((prev) => !prev)}
+        className="flex gap-1 items-center cursor-pointer"
+      >
+        <div className="w-4">
+          {!open && <PlusCircleIcon />}
+          {open && <XCircleIcon />}
+        </div>
         <div>
-          <Button onClick={save} className="cursor-pointer w-48" color={"green"}>
-            Save
-          </Button>
+          New Card in <span className="font-bold">{category}</span> category
         </div>
       </div>
+
+      {open && (
+        <>
+          <div className="mt-2">
+            <Field>
+              <Label>Question</Label>
+              <Textarea
+                value={question}
+                onChange={(e) => setQuestion(e.target.value)}
+              />
+            </Field>
+          </div>
+
+          <div className="mt-2">
+            <Field>
+              <Label>Answer</Label>
+              <Textarea
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+              />
+            </Field>
+          </div>
+
+          <div className="mt-2 flex justify-end">
+            <div>
+              <Button
+                onClick={save}
+                className="cursor-pointer w-48"
+                color={"green"}
+              >
+                Save
+              </Button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
